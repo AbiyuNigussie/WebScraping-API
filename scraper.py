5ed4820c99c8ccc10c80d20a01cfe83d37b9c29d
AbiyuTolosa <abiyunigussie7@gmail.com>
2022-09-12 13:03:01 +0300

    scrapper finished

--- scraper.py
+++ scraper.py
@@ -1,1 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.listchallenges.com/100-random-foods"
+
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, 'lxml')
+
+lists = soup.find('div', attrs={'id': 'repeaterListItems'})
+datas = lists.find_all('div', attrs={'class': 'item-name'})
+
+foods = [data.text.strip() for data in datas]
+
+
